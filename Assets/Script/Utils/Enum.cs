bcd9a02864d3670b853bc3f7b7fad65f517aec4e
PedroGomes15 <pedro.armando15@hotmail.com>
2020-11-04 22:07:50 -0300

    Start

---
+++ Assets/Script/Utils/Enum.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enum : MonoBehaviour
+{
+    enum tileType
+    {
+        property,
+        jail,
+        shares,
+        card,
+        start,
+        gotoJail,
+        luckyTry,
+        none
+    };
+}
+
+

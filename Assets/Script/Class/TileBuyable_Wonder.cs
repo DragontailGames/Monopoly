72b25b7250eecef4f6906abd247a96888356c8d6
PedroGomes15 <pedro.armando15@hotmail.com>
2020-11-20 21:47:53 -0300

    seila

---
+++ Assets/Script/Class/TileBuyable_Wonder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Tile Wonder", menuName = "Monopoly/Tile Wonder", order = 3)]
+public class TileBuyable_Wonder : TileBuyable
+{
+    public Sprite flag;
+}

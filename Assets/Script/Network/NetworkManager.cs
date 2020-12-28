1eaa3137da2ab4795bbf7332f32021f748a93b27
PedroGomes15 <pedro.armando15@hotmail.com>
2020-12-28 19:00:06 -0300

    Photon

---
+++ Assets/Script/Network/NetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using ExitGames.Client.Photon;
+using UnityEngine.Events;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Utils/EnumDt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumDt : MonoBehaviour
+{
+    enum tileType
+    {
+        property,
+        jail,
+        shares,
+        card,
+        start,
+        gotoJail,
+        luckyTry,
+        none
+    };
+
+    public enum tradingBlock
+    {
+        sulamericano,
+        americano,
+        europeu,
+        asiatico,
+        africano,
+        orientemedio,
+        eslavo,
+        oceanico
+    };
+}
+
+

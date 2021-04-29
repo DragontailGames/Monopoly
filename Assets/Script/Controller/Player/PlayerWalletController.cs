78403a2d2daac542c9d736712950683ecc7f8750
PedroGomes15 <pedro.armando15@hotmail.com>
2021-04-29 10:42:09 -0300

    Mais fix

--- Assets/Script/Controller/Player/PlayerWalletController.cs
+++ Assets/Script/Controller/Player/PlayerWalletController.cs
@@ -122,9 +122,9 @@ public class PlayerWalletController : MonoBehaviour
     {
         TileController_Buyable tile = controller.manager.board.tileControllers.Find(n => n.index == index) as TileController_Buyable;
 
-        Debug.Log("Comprando " + index);
+        Debug.Log("Comprando " + index + " - " + ((controller.player != null && !controller.player.IsLocal) || controller.botController));
 
-        if((controller.player != null && !controller.player.IsLocal) || controller.botController)
+        if ((controller.player != null && !controller.player.IsLocal) || controller.botController)
             MessageManager.Instance.ShowMessage(controller.name + " comprou uma propriedade.");
 
         tile.Owner = controller;

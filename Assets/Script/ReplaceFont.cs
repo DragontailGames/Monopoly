7250d27e09b42a71cb1bc0a484ca11e7b8a411d0
Pedro Gomes <pedro.armando15@hotmail.com>
2021-08-31 23:33:50 -0300

    Replace font

---
+++ Assets/Script/ReplaceFont.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ReplaceFont : MonoBehaviour
+{
+    public Font newFont;
+    public TMP_FontAsset newFontTMP;
+
+    [ContextMenu("Replace")]
+    public void Replace()
+    {
+        foreach(TextMeshProUGUI txt in FindObjectsOfType<TextMeshProUGUI>())
+        {
+            txt.font = newFontTMP;
+        }
+        foreach (Text txt in FindObjectsOfType<Text>())
+        {
+            txt.font = newFont;
+        }
+    }
+}
